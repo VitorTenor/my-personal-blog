295b120ad580b1c29777de283ae352b122d070fb
Vitor Tenório de Lima <vitortenorio35@gmail.com>
2023-10-20 04:41:35 -0300

    Ajuste

--- src/ui/molecule/AnimatedParagraph/index.tsx
+++ src/ui/molecule/AnimatedParagraph/index.tsx
@@ -1,25 +1,24 @@
-import {ReactElement} from "react";
-import {animated, useSpring} from "react-spring";
+import  { ReactElement } from "react";
+import { animated, useSpring, config } from "react-spring";
 import Paragraph from "../../atom/Paragraph";
-
 
 interface AnimatedParagraphProps {
     message?: string;
     element?: ReactElement;
 }
 
-
-function AnimatedParagraph(props : AnimatedParagraphProps) : ReactElement {
+function AnimatedParagraph(props: AnimatedParagraphProps): ReactElement {
     const shakeAnimation = useSpring({
-        from: { transform: "translate(50px, 50px)" },
-        to: { transform: "translate(5px, 5px)" },
-        config: { duration: 400 },
-        loop:{ reverse: false },
+        from: { transform: "translate(10px, 0px)" },
+        to: { transform: "translate(0px, 0px)" },
+        config: { ...config.default, duration: 400 },
+        reset: true, // Define para reverter para o estado inicial
+        reverse: false,
     });
 
     return (
         <animated.p style={shakeAnimation}>
-            {props.element ? props.element : <Paragraph message={props.message ? props.message : ""} /> }
+            {props.element ? props.element : <Paragraph message={props.message ? props.message : ""} />}
         </animated.p>
     );
 }
