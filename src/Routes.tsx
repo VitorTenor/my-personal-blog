b19ddef0e53e6a82b6cb8acf2985663d3c5e8ee7
Vitor Tenório de Lima <vitortenorio35@gmail.com>
2023-10-19 23:50:59 -0300

    Criando pagina main e separando default vite

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,5 +1,6 @@
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import MainPage from "./pages/main";
+import ViteDefault from "./pages/viteDefault";
 
 
 export default function Routes() {
@@ -8,6 +9,10 @@ export default function Routes() {
             path: "/",
             element: <MainPage/>
         },
+        {
+            path: "/vite",
+            element: <ViteDefault/>
+        }
     ]);
 
     return (
