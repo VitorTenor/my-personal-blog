0972531dd35744d22404ee123f61218fba000bbc
Vitor Tenório de Lima <vitortenorio35@gmail.com>
2023-10-19 23:46:57 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Routes from "./Routes.tsx";
+
+function App() {
+
+  return (
+      <Routes />
+  )
+}
+
+export default App
---
+++ src/Routes.tsx
@@ -0,0 +1,17 @@
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import MainPage from "./pages/main";
+
+
+export default function Routes() {
+    const routeList = createBrowserRouter([
+        {
+            path: "/",
+            element: <MainPage/>
+        },
+    ]);
+
+    return (
+            <RouterProvider router={routeList} />
+    );
+
+}
