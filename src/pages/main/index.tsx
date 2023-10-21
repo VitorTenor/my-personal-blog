60ce1b6f684e10de5a5f8835c787f7cf35d4008f
Vitor Tenório de Lima <vitortenorio35@gmail.com>
2023-10-21 00:39:50 -0300

    rollback session 2

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -2,13 +2,11 @@ import {ReactElement} from "react";
 
 import {MainPageStyles} from "./styles.ts";
 import Section1 from "../../ui/organism/Section[1]";
-import Section2 from "../../ui/organism/Section[2]";
 
 export default function MainPage(): ReactElement {
     return (
         <MainPageStyles>
             <Section1 id={"section1"}/>
-            <Section2 id={"section2"}/>
         </MainPageStyles>
     );
 }
