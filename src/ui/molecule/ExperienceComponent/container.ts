b8409a5b65722b8af877852166016c803d954689
vitor tenorio <vitortenorio35@gmail.com>
2024-02-21 21:59:24 -0300

    Update container.ts

--- src/ui/molecule/ExperienceComponent/container.ts
+++ src/ui/molecule/ExperienceComponent/container.ts
@@ -9,3 +9,19 @@ export const DESCRIPTION_PREFIX: string = '$ ';
 export const SPAN_STYLE: CSSProperties = {
   color: `${globalColor.quaternary}`,
 };
+
+export interface ExperienceComponentProps {
+  title: string;
+  date: string;
+  company: string;
+  description: string[];
+}
+
+export interface ExperienceElementProps {
+  path: string;
+  experience: ExperienceComponentProps[];
+}
+
+export interface ExperienceProps {
+  childrens: ExperienceElementProps[];
+}
