e0cbac8f4391928aa021341bbe8dfb0d60944724
vitor tenorio <vitortenorio35@gmail.com>
2024-04-25 01:41:06 -0300

    Using backend to load languages => build error

--- src/language/languageConfig.ts
+++ src/language/languageConfig.ts
@@ -2,20 +2,15 @@ import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import { AvailableLanguages, getLanguage } from './languageService.ts';
 
-const resources : {} = {
-  PT_BR: {
-    translation: await getLanguage(AvailableLanguages.PT_BR)
-  },
-  EN_US: {
-    translation: await getLanguage(AvailableLanguages.EN_US)
-  }
+
+const loadResources = async () => {
+  i18next.addResourceBundle(AvailableLanguages.PT_BR, 'translation', await getLanguage(AvailableLanguages.PT_BR));
+  i18next.addResourceBundle(AvailableLanguages.EN_US, 'translation', await getLanguage(AvailableLanguages.EN_US));
 }
 
 i18next
   .use(initReactI18next)
   .init({
-    resources: resources,
-    lng: 'PT_BR',
-    fallbackLng: 'EN_US'
+    lng: AvailableLanguages.PT_BR
   })
-  .then();
+  .then(loadResources);
