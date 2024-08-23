1987879f7710fba983710a00e3dfc946b0367ab2
Vitor Tenório <79546333+VitorTenor@users.noreply.github.com>
2024-08-23 12:01:53 -0300

    [ContactsSection] : removing instagram link and changing mail icon.

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,4 +1,3 @@
 export const GITHUB_LINK: String = 'https://github.com/VitorTenor';
 export const LINKEDIN_LINK: String = 'https://www.linkedin.com/in/vitortelima/';
-export const INSTAGRAM_LINK: String = 'https://www.instagram.com/ovitortenorio';
 export const EMAIL_LINK: String = 'mailto:vitortenorio.dev@gmail.com';
